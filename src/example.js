ad83e8ea9ee41abe0e4e813ae5f7e76cb3f866cd
John Rees <john@bitsushi.com>
2017-02-23 17:21:48 +0000

    updated example

--- src/example.js
+++ src/example.js
@@ -13,8 +13,8 @@ const bots = [
   new Bot('Add4', add, { X: 'Add3>SUM', Y: 10 }),
   new Bot('Add5', add, { X: 'Add3>SUM', Y: 'Add4>SUM' }),
   new Bot('Add6', add, { X: 'Add1>SUM', Y: 'Add3>SUM' }),
-  new Bot('Add7', add, { X: 'Add5>SUM', Y: 'Add6>SUM' }),
-  new Bot('Print', prettyPrint, { IN: 'Add7>SUM' })
+  new Bot('Add7', add, { X: 'Add6>SUM', Y: 'Add5>SUM' }),
+  new Bot('Print1', prettyPrint, { IN: 'Add7>SUM' })
 ]
 
 bots.map(graph.addBot)
