0b756e27939df3d6e669a0276f6dd38dd6ab2420
John Rees <john@bitsushi.com>
2017-06-06 17:58:40 +0100

    more examples

--- examples/basic.js
+++ examples/basic.js
@@ -1,20 +1,21 @@
-import { Graph, Bot } from '../lib'
-import colors from 'colors/safe'
+// import { Graph, Bot } from '../lib'
+var BentoBots = require("../lib")
+var colors = require('colors/safe')
 
-var graph = new Graph({name: 'Test Graph'})
+var graph = new BentoBots.Graph({name: 'Test Graph'})
 
 const add = { implementation: ({ X = 1, Y = 1 }) => ({ SUM: X + Y }) }
 const prettyPrint = { implementation: ({ IN }) => console.log(colors.rainbow(`The result is: ${IN}`)) }
 
 const bots = [
-  new Bot('Add1', add, { X: 3, Y: 3 }),
-  new Bot('Add2', add, { X: 20, Y: 10 }),
-  new Bot('Add3', add, { X: 'Add1>SUM', Y: 10 }),
-  new Bot('Add4', add, { X: 'Add3>SUM', Y: 10 }),
-  new Bot('Add5', add, { X: 'Add3>SUM', Y: 'Add4>SUM' }),
-  new Bot('Add6', add, { X: 'Add1>SUM', Y: 'Add3>SUM' }),
-  new Bot('Add7', add, { X: 'Add6>SUM', Y: 'Add5>SUM' }),
-  new Bot('Print1', prettyPrint, { IN: 'Add7>SUM' })
+  new BentoBots.Bot('Add1', add, { X: 3, Y: 3 }),
+  new BentoBots.Bot('Add2', add, { X: 20, Y: 10 }),
+  new BentoBots.Bot('Add3', add, { X: 'Add1>SUM', Y: 10 }),
+  new BentoBots.Bot('Add4', add, { X: 'Add3>SUM', Y: 10 }),
+  new BentoBots.Bot('Add5', add, { X: 'Add3>SUM', Y: 'Add4>SUM' }),
+  new BentoBots.Bot('Add6', add, { X: 'Add1>SUM', Y: 'Add3>SUM' }),
+  new BentoBots.Bot('Add7', add, { X: 'Add6>SUM', Y: 'Add5>SUM' }),
+  new BentoBots.Bot('Print1', prettyPrint, { IN: 'Add7>SUM' })
 ]
 
 bots.map(b => graph.addBot(b))
---
+++ examples/designscript.js
@@ -0,0 +1,47 @@
+// import { Graph, Bot } from '../lib'
+var BentoBots = require("../lib")
+
+var graph = new BentoBots.Graph({name: 'Designscript example'})
+
+const codeBlock = { implementation: () => ({ A: 0, B: 20, C: 4 }) }
+
+const range = {
+  implementation: ({ START, END, STEP }) => {
+    let SEQ = []
+    for(var i = START; i <= END; i+= STEP) {
+      SEQ.push(i)
+    }
+    return { SEQ }
+  }
+}
+
+const prettyPrint = { implementation: ({ IN }) => console.log(IN) }
+
+const pointByCoords = {
+  implementation: ({ X, Y, Z }) => {
+    let POINT = []
+    const a = val => Array.isArray(val) ? val : [val]
+
+    // should be returned as 2D array http://i.imgur.com/5KfmIJU.png
+    a(X).forEach(_X => {
+      a(Y).forEach(_Y => {
+        a(Z).forEach(_Z => {
+          POINT.push({ X: _X, Y: _Y, Z: _Z })
+        })
+      })
+    })
+    return { POINT }
+  }
+}
+
+const bots = [
+  new BentoBots.Bot('CodeBlock', codeBlock, {}),
+  new BentoBots.Bot('Range', range, { START: 'CodeBlock>A', END: 'CodeBlock>B', STEP: 'CodeBlock>C' }),
+  new BentoBots.Bot('Print1', prettyPrint, { IN: 'Range>SEQ' }),
+  new BentoBots.Bot('Point', pointByCoords, { X: 'Range>SEQ', Y: 'Range>SEQ', Z: 0 }),
+  new BentoBots.Bot('Print2', prettyPrint, { IN: 'Point>POINT' })
+]
+
+bots.map(b => graph.addBot(b))
+
+graph.run()
